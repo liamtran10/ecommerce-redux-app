fdc8130815ed77d6953440c2086eea8ef48326c2
Liam Tran <liamtran2000@gmail.com>
2022-05-31 13:30:22 +1000

    Created and rendered components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import Header from './components/Header';
+import ProductListing from './components/ProductListing';
+import ProductDetail from './components/ProductDetail';
 
 function App() {
   return (
-    <div>
-      <h2>Hello, world</h2>
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
